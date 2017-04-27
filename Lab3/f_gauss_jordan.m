f9c5d76fbb9b09cc5bc270f2251e33af2ff2611c
Mariusz Gumienny <gumienny@users.noreply.github.com>
2017-04-27 20:03:33 +0200

    zaimplementowano metodę Gaussa-Jordana

--- Lab3/f_gauss_jordan.m
+++ Lab3/f_gauss_jordan.m
@@ -1,3 +1,16 @@
 function [ x ] = f_gauss_jordan( A, b )
-    % zaimplementowaæ metodê Gaussa-Jordana  
+	Ar = [ A, b ];
+	[ N, null ] = size( Ar );
+
+	for kk = 1:1:N
+		Ar(kk,:) /= Ar(kk,kk);
+
+		for ii = 1:1:N
+			if ( ii != kk )
+				Ar(ii,:) = Ar(ii,:) - Ar(ii,kk)*Ar(kk,:);
+			endif
+		endfor
+	endfor
+
+	x = Ar( :, N + 1)'; 
 endfunction
