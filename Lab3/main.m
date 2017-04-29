b0f36b779253a4894e93da6e818da401cc129b21
Mariusz Gumienny <gumienny@users.noreply.github.com>
2017-04-29 14:05:06 +0200

    Update main.m

--- Lab3/main.m
+++ Lab3/main.m
@@ -2,16 +2,16 @@ more off;
 clc;
 
 % 1.
+% A - kwadratowa macierz współczynników
+% b - wektor wyrazów wolnych
 A = [ 3, 1, -1; -1, 5, -1; 2, 4, 8 ];
 b = [ 6; 10; 2 ];
 
 % 2.
 disp( 'A\b' )
-x = A \ b
+x = ( A \ b ).'
 
 % 3., 4.
-% A - kwadratowa macierz współczynników
-% b - wektor wyrazów wolnych
 % x - wektor szukanych (rozwiązanie URL)
 % Ur - macierz rozszerzona układu przekształcona do macierzy trójkątnej górnej
 disp( 'Metoda Gaussa:' )
@@ -22,12 +22,10 @@ disp( 'Rozklad LU - natywna funkcja:' );
 [ L, U ] = lu( A )
 
 % 6., 7.
-disp( 'Rozklad LU - z zaimplementowanych metod:' )
+disp( 'Rozklad LU - z zaimplementowanej metody Gaussa-Doolittlea:' )
 [ L, U ] = f_gauss_doolittle( A )
 
 % 8., 9.
-% A - kwadratowa macierz współczynników
-% b - wektor wyrazów wolnych
 % k_max - maksymalna ilość iteracji
 % tol - żądana dokładność rozwiązania
 % xx - wektor szukanych (rozwiązanie URL)
@@ -38,8 +36,6 @@ disp( 'Metoda Jacobiego:' )
 [ xx, kk ] = f_jacobi( A, b, k_max, tol )
 
 % 10.
-% A - kwadratowa macierz współczynników
-% b - wektor wyrazów wolnych
 % x - wektor szukanych (rozwiązanie URL)
 disp( 'Metoda Gaussa-Jordana:' )
 [ x ] = f_gauss_jordan( A, b )
