54d8aaff5c5e0b3a3c98727808e857dbcbef791d
Mariusz Gumienny <gumienny@users.noreply.github.com>
2017-05-10 09:52:25 +0200

    Update f_lagrange.m

--- Lab4/f_lagrange.m
+++ Lab4/f_lagrange.m
@@ -1,16 +1,12 @@
 function [ y ] = f_lagrange( x0, y0, x )
-    n = size( x0, 2 );
-    y = 0;
+    y = zeros( size( x ) );
 
-    for i = 1:n
-        L( i, 1:size( x, 2 ) ) = 1;
-
-        for j = 1:n
-            if ( i ~= j )
-                L( i, : ) = L( i, : ) .* ( x - x0( j ) ) / ( x0( i ) - x0( j ) );
-            end
+    for i = 1:length( x0 )
+        L = 1;
+        for j = [ 1:( i-1 ), ( i+1 ):length( x0 ) ]
+            L = L .* ( x - x0( j ) ) / ( x0( i ) - x0( j ) );
         end
 
-        y = y + y0( i ) * L( i, : );
+        y = y + y0( i ) * L;
     end
 end
