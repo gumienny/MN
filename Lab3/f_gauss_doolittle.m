9e9aa7ea3df91d821c2149a7c48e345ad37da985
Mariusz Gumienny <gumienny@users.noreply.github.com>
2017-05-26 21:02:43 +0200

    Create f_gauss_doolittle.m

---
+++ Lab3/f_gauss_doolittle.m
@@ -0,0 +1,27 @@
+function [ L, U ] = f_gauss_doolittle( A )
+    N = size( A )( 1 );
+    
+    for kk = 1:N
+        L( kk, kk ) = 1;
+
+        for jj = kk:N
+            sum = 0;
+
+            for ss = 1:kk-1
+                sum = sum + L( kk, ss ) * U( ss, jj );
+            end
+
+            U( kk, jj ) = A( kk, jj ) - sum;
+        end
+
+        for ii = kk+1:N
+            sum = 0;
+
+            for ss = 1:kk-1
+                sum = sum + L( ii, ss ) * U( ss, kk );
+            end
+
+            L( ii, kk ) = ( A( ii, kk ) - sum ) / U( kk, kk );
+        end
+    end
+end
