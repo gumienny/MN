0d24613018ef74c725c0aabae36e142bba417bb3
Mariusz Gumienny <gumienny@users.noreply.github.com>
2017-06-18 09:54:47 +0200

    remove comments :|

--- Lab2/f_bisect.m
+++ Lab2/f_bisect.m
@@ -1,46 +1,39 @@
-function [c, yc, OUT] = f_bisect(fun, a, b, k_max, tol)
-    % Obliczenie wartosci funkcji na krancach przedzialu
-    ya = eval([fun '(a)']) ; % <=> fun(a) ;
-    yb = eval([fun '(b)']) ; % <=> fun(b) ;
-    
-    % Sprawdzenie czy funkcja ma rozne znaki na krancach przedzialu poczatkowego
-    if (sign(ya) == sign(yb)) % if (ya*yb>0)
+function [ c, yc, OUT ] = f_bisect( fun, a, b, k_max, tol )
+    ya = eval( [fun '(a)'] );
+    yb = eval( [fun '(b)'] );
+
+    if ( sign( ya ) == sign( yb ) )
         error('Funkcja nie ma roznych znakow na koncach przedzialu.')
     endif
     
     disp('Krok      a    b    c     yc  blad') ;
     
     for ii=1:1:k_max
-        % Obliczenie kolejnego przyblizenia pierwiastka
-        c = (a+b)/2;
-    
-        % Obliczenie wartosci funkcji dla ii-tego przyblizenia pierwiastka
-        yc = eval([fun '(c)']) ; % fun1(c) ;
-        
-        blad = (b-a)/2 ;
-        
-        % Wyswietlenie informacji dotyczacych ii-tej iteracji
-        out = [ii a b c yc blad]; 
-        disp(out);
-        OUT(ii,:) = out ;
-        
-        % Sprawdzenie zbieznosci metody
-        if ( abs(yc) < tol )
+        c = ( a + b ) / 2;
+
+        yc = eval( [fun '(c)'] );
+
+        blad = ( b - a ) / 2;
+
+        out = [ ii a b c yc blad ]; 
+        disp( out );
+        OUT( ii, : ) = out;
+
+        if ( abs( yc ) < tol )
             disp( ['Metoda bisekcji zbiezna po ' num2str(ii) 'krokach.'] )
-            break ;
-        endif
-                
-        % Wyznaczenie granic przedzialu dla kolejnej iteracji
-        if ( sign(ya) == sign(yc) )
-            a = c ;     ya = yc ;
-        else
-            b = c ;   yb = yc ; 
+            break;
         endif
 
-        % Wyswietlenie komunikatu w przypadku gdy przyblizenia z zadana dokladnoscia
-        % nie odnaleziono w zadanej maksymalnej liczbie iteracji
-        if (ii >= k_max)
-            disp('Nie odnaleziono zera funkcji z zadana dokladnoscia.')
+        if ( sign( ya ) == sign( yc ) )
+            a = c;     
+            ya = yc;
+        else
+            b = c;
+            yb = yc; 
+        endif
+
+        if ( ii >= k_max )
+            disp( 'Nie odnaleziono zera funkcji z zadana dokladnoscia.' )
         endif
     endfor
 endfunction
